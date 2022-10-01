7a1f1809854b95350affd1053a1123b70d24c77b
Jason Zukiewicz <zukiewiczjason@gmail.com>
2022-10-01 18:32:16 -0400

    block

---
+++ Assets/Scripts/Custom/block.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class block : MonoBehaviour
+{
+    public GameObject cube;
+    private Renderer rend;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // This will get the OBJECT to CHANGE COLOR on KEY PRESS
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+            print("A Key Pressed For Red Color");
+        }
+        if (Input.GetKeyDown(KeyCode.B)) { 
+            GetComponent<Renderer>().material.color = Color.blue;
+            print("A Key Pressed For Blue Color");
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision");
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.name == "Player_1")
+        {
+            print("Flag 1");
+            transform.GetComponent<Renderer>().material.color = Color.blue;
+        }
+        else if (collision.gameObject.name == "Player_2")
+        {
+            transform.GetComponent<Renderer>().material.color = Color.green;
+        }
+
+    }
+
+}
